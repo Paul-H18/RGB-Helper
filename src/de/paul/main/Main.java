62b7a426e7733e4881555c6750cc2a813ed80948
Paul <66725843+Paul-H18@users.noreply.github.com>
2022-10-17 18:16:31 +0200

    Added R,G,B output below the JPanel

--- src/de/paul/main/Main.java
+++ src/de/paul/main/Main.java
@@ -11,7 +11,12 @@ public class Main {
 
     public static void main (String[] args) {
 
-        Color frameBg = Color.DARK_GRAY, frameFg, buttonBg = Color.GRAY, text = Color.WHITE;
+        Color frameBg = Color.DARK_GRAY, frameFg, buttonBg = new Color(178, 178, 178), text = Color.WHITE, errorText = Color.RED;
+
+        Font mainFont = new Font("Times New Roman", Font.PLAIN, 14);
+
+        ImageIcon imgIco = new ImageIcon("src/triangle.png");
+        Image img = imgIco.getImage();
 
         int rgb[] = new int[3];
 
@@ -19,7 +24,7 @@ public class Main {
 
         frame.setVisible(true);
         frame.setLayout(null);
-        frame.setBounds(0,0, 400, 300);
+        frame.setBounds(0,0, 450, 310);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
@@ -27,25 +32,38 @@ public class Main {
 
         frame.setResizable(false);
         frame.setTitle("Color");
+        frame.setIconImage(img);
 
         JPanel jp = new JPanel();
 
 
         JTextField[] fields = new JTextField[3];
         JLabel[] labels = new JLabel[3];
+        JLabel[] display = new JLabel[3];
 
         for (int i = 0; i < 3; i++) {
             fields[i] = new JTextField();
             labels[i] = new JLabel();
 
+            display[i] = new JLabel();
+
             fields[i].setVisible(true);
             labels[i].setVisible(true);
+
+            display[i].setVisible(true);
 
             fields[i].setBackground(buttonBg);
             fields[i].setForeground(text);
             fields[i].setBorder(new LineBorder(buttonBg));
 
+            fields[i].setFont(mainFont);
+            labels[i].setFont(mainFont);
+
             labels[i].setForeground(text);
+
+            display[i].setFont(mainFont);
+
+            display[i].setForeground(text);
 
         }
 
@@ -61,6 +79,19 @@ public class Main {
         labels[2].setText("B:");
         labels[2].setBounds(10, 90, 30, 30);
 
+
+
+
+
+        display[0].setBounds(200, 185, 60, 30);
+        display[0].setText("R: " + rgb[0]);
+
+        display[1].setBounds(270, 185, 60, 30);
+        display[1].setText("G: " + rgb[1]);
+
+        display[2].setBounds(340, 185, 60, 30);
+        display[2].setText("B: " + rgb[2]);
+
         JButton button = new JButton();
         button.setText("Display");
         button.setBounds(50, 130, 120, 30);
@@ -73,17 +104,26 @@ public class Main {
 
                 for (int i = 0; i < 3; i++) {
                     if(Integer.parseInt(fields[i].getText()) <= 255 && Integer.parseInt(fields[i].getText()) >= 0) {
+                        fields[i].setForeground(text);
                         rgb[i] = Integer.parseInt(fields[i].getText());
+
+                    } else {
+                        fields[i].setForeground(errorText);
                     }
-                    System.out.println(rgb[i]);
+                    display[0].setText("R: " + rgb[0]);
+                    display[1].setText("G: " + rgb[1]);
+                    display[2].setText("B: " + rgb[2]);
+
+                    System.out.print(rgb[i] + "; ");
                 }
+                System.out.println();
                 jp.setBackground(new Color(rgb[0], rgb[1], rgb[2]));
 
             }
         });
 
         jp.setBackground(Color.black);
-        jp.setBorder(new LineBorder(Color.GRAY));
+        jp.setBorder(new LineBorder(Color.WHITE));
         jp.setBounds(200, 10, 160, 160);
 
 
@@ -95,6 +135,7 @@ public class Main {
         for (int i = 0; i < 3; i++) {
             frame.add(fields[i]);
             frame.add(labels[i]);
+            frame.add(display[i]);
         }
         frame.add(jp);
         frame.add(button);
